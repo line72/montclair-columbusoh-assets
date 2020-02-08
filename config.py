374859892a283d3ec8c074117d6505a04d86f81b
Marcus Dillavou <line72@line72.net>
2020-02-08 09:28:37 -0600

    Rebuild android and ios with updated name

--- config.py
+++ config.py
@@ -19,14 +19,14 @@ CONFIG = transmogrifier.Config(
     ),
     ios_config = transmogrifier.MontclairiOSConfig(
         version = '2.0.5',
-        revision = 1,
+        revision = 2,
         app_id = 'com.gotransitapp.columbusoh',
         app_store_id = 'REPLACE_ME',
         app_store_url = 'https://apps.apple.com/us/app/REPLACE_ME'
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
         version = '1.0.2',
-        revision = 1,
+        revision = 2,
         app_id = 'com.gotransitapp.columbusoh',
         play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.columbusoh'
     )
